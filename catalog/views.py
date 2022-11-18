457077f513863e2a10f2fd52ad1d5dd394fcdb40
Richard Khillah <richardkhillah@Richards-MacBook-Air.local>
2022-11-17 21:47:20 -0800

    customer view first installment

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from .models import Book, Author, BookInstance, Genre
 
@@ -30,3 +31,9 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+
+class BookDetailView(generic.DetailView):
+    model = Book
