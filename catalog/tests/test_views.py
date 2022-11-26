7a9c1d83961b3ceb3231fd229fc54b148d59e2e8
Richard Khillah <richardkhillah@Richards-MacBook-Air.local>
2022-11-25 21:09:04 -0800

    AuthorListViewTest

--- catalog/tests/test_views.py
+++ catalog/tests/test_views.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from catalog.models import Author
+
+class AuthorListViewTest(TestCase):
+    @classmethod
+    def setUp(self):
+        # Create 13 authors for pagination tests
+        number_of_authors = 13
+        for author_id in range(number_of_authors):
+            Author.objects.create(
+                first_name=f'Dominique {author_id}',
+                last_name=f'Surname {author_id}',
+            )
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/catalog/authors/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('authors'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('authors'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'catalog/author_list.html')
+
+    def test_pagination_is_ten(self):
+        response = self.client.get(reverse('authors'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.context['is_paginated'] == True)
+        self.assertEqual(len(response.context['author_list']), 5)
+
+    def test_list_all_authors(self):
+        # Get third page and confirm it has (exactly) remaining 3 items
+        response = self.client.get(reverse('authors')+'?page=3')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('is_paginated' in response.context)
+        self.assertTrue(response.context['is_paginated'] == True)
+        self.assertEqual(len(response.context['author_list']), 3)
